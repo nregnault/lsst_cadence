e9cc60a34146c6767fc0d050fbeaf3ec97344171
Nicolas Regnault <nicolas.regnault@lpnhe.in2p3.fr>
2018-04-19 21:27:36 +0200

    utility to dump salt2 light curve templates

---
+++ tools/dump_salt2_templates.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import os.path as op
+import sys
+
+import numpy as np
+import pylab as pl
+import argparse
+import logging
+logging.basicConfig(fmt="%(asctime)s %(levelname)s %(message)",
+                    level=logging.INFO)
+from saunerie import salt2, snsim
+from pycosmo.cosmo import CosmoLambda
+
+cosmo = CosmoLambda()
+
+
+def get_pulse_shapes(bands,
+                     X1=-2., Color=0.2, DayMax=0.,                     
+                     restframe_phase_range=(-20., 45.),
+                     redshifts=np.arange(0.05, 1.2, 0.05),
+                     filename='salt2.npz'):
+    """
+    """
+    lcmodel = snsim.init_lc_model(bands, filename=filename)
+    pmin, pmax = restframe_phase_range
+
+    l = []
+    for z in redshifts:
+        mjd_min = np.floor(pmin * (1+z) + DayMax)
+        mjd_max = np.ceil(pmax * (1+z) + DayMax)
+        mjd = np.arange(mjd_min, mjd_max+1, 1.)
+        sn = np.rec.fromrecords([(z, cosmo.dL(z), X1, Color, DayMax)],
+                                names=['z', 'dL', 'X1', 'Color', 'DayMax'])
+        n = len(mjd)
+        o = np.ones(n)
+        for bn in bands:
+            b = [bn] * n
+            val = lcmodel(sn, mjd, b)
+            l.append(np.rec.fromarrays((mjd, val, z*o, b),
+                                       names=['mjd', 'val', 'z', 'band']))
+            
+    return np.hstack(l)
+    
+
+
+if __name__ == "__main__":
+    pass
